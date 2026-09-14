05f0af9801f91bb74a371ebb72e5a8f66cd77274
sun <2396825244@qq.com>
2026-09-14 22:17:45 +0800

    Initial commit: bind sunwanqing folder to Gitee repo

---
+++ vue-login-homework/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Login></Login>
+</template>
+
+<script setup>
+import Login from './Login.vue'
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
